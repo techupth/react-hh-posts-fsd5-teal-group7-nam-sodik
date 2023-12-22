db06819b00b9a79560469c1c793eb6788df58349
shnItt1 <144312080+shnItt1@users.noreply.github.com>
2023-12-22 17:04:12 +0700

    add hh-post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,63 @@
+import { useState } from "react";
+import postData from "../data/postdata";
+// 1. import useState
 function Posts() {
+  // 2. สร้าง State กำหนดค่าตั้งต้นเป็นข้อมูลที่มี
+  const [like, setLike] = useState(postData);
+
+  const thumbUp = (likeIndex) => {
+    // 4. เขียน logic ในการอัพเดท like state ตอนนี้มันคือ [{},{}] > Clone Array > reassign ตัว property likesตามindex ให้บวกเพิ่ม > อัพเดทตัว stateเป็นค่าใหม่
+    const addNewLike = [...like];
+    addNewLike[likeIndex].likes += 1;
+    setLike(addNewLike);
+  };
+
+  const thumbDown = (likeIndex) => {
+    const addNewLike = [...like];
+    addNewLike[likeIndex].likes > 0
+      ? (addNewLike[likeIndex].likes -= 1)
+      : (addNewLike[likeIndex].likes = 0);
+    setLike(addNewLike);
+  };
+
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+        {like.map((item, index) => {
+          // เอา like มา render เป็น list item ด้วย .map() มีparam 1ตัว
+          // state อยู่ใน arrayของobject
+          return (
+            <div key={index} class="post-item">
+              <div class="post-header">
+                <h2>{item.title}</h2>
+                <div class="post-social-media-stats">
+                  <span class="stats-topic">Likes: </span>
+                  <span class="post-likes">{item.likes}</span>
+                </div>
+              </div>
+              <p class="post-content">{item.content}</p>
+              <div class="post-actions">
+                <button
+                  class="like-button"
+                  onClick={() => {
+                    thumbUp(index);
+                  }}
+                >
+                  Like
+                </button>
+                <button
+                  class="dislike-button"
+                  onClick={() => {
+                    thumbDown(index);
+                  }}
+                >
+                  Dislike
+                </button>
+              </div>
             </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
